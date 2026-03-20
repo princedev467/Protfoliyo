1ac27d132b343af2f54db732fbcea53aa7156652
prince476-web <princemovaliya467@gmail.com>
2026-03-20 19:17:03 +0530

    ss

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,10 @@
 import React from 'react';
-import Demo from './Demo/Demo';
-import Nevbar from './Demo/Nevbar';
-import Styletai from './Styletai/Styletai';
-import Header from './Header/Header';
-import GridDemo from './Demo/GridDemo';
+
 import { ThemeProvider } from './context/theme.context';
 import { Route, Routes } from 'react-router-dom';
 import UserRouts from './Routes/UserRouts';
 import PrivateRouts from './Routes/PrivateRouts';
 import AdminRouts from './Routes/AdminRouts';
-import Animation from './Demo/animation';
 
 function App(props) {
   return (
