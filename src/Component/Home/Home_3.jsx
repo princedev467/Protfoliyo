8859574c81a9e933bb783a3e22499e4db4c0bbef
prince476-web <princemovaliya467@gmail.com>
2026-03-26 18:38:15 +0530

    okok

--- src/Component/Home/Home_3.jsx
+++ src/Component/Home/Home_3.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import Profile from '../../assets/images/profile/personalPhoto.png'
 import { FaReact, FaNodeJs, FaGitAlt } from "react-icons/fa";
-import { SiMongodb, SiTailwindcss, SiJavascript, SiExpress,SiC  } from "react-icons/si";
+import { SiMongodb, SiTailwindcss, SiJavascript, SiExpress, SiC } from "react-icons/si";
 import { TbApi } from "react-icons/tb";
 import { TiHtml5 } from "react-icons/ti";
 import { FaCss3Alt } from "react-icons/fa";
+import cv from '../../assets/images/profile/cv.pdf'
 
 function Home_3(props) {
     const skills = [
@@ -12,25 +13,24 @@ function Home_3(props) {
         { name: "Node JS", icon: <FaNodeJs className='text-green-500 size-12' /> },
         { name: "MongoDB", icon: <SiMongodb className='text-green-600 size-12' /> },
         { name: "Tailwind CSS", icon: <SiTailwindcss className='text-sky-400 size-12' /> },
-        { name: "JavaScript", icon: <SiJavascript className='text-yellow-400 size-12' /> },
-        { name: "Git & GitHub", icon: <FaGitAlt className='text-orange-500 size-12' /> },
+        { name: "JavaScript", icon: <SiJavascript className='text-yellow-400 size-12  bg-black' /> },
+        { name: "Git & GitHub", icon: <FaGitAlt className='text-orange-500 size-12 ' /> },
         { name: "REST API", icon: <TbApi className='text-purple-400 size-12' /> },
         { name: "Express JS", icon: <SiExpress className='text-gray-400 size-12' /> },
         { name: "HTML", icon: <TiHtml5 className='text-orange-600 size-12' /> },
         { name: "CSS", icon: <FaCss3Alt className='text-blue-500 size-12' /> },
-        { name: "C", icon: <SiC  className='text-blue-600 size-12' /> },
+        { name: "C", icon: <SiC className='text-blue-600 size-12' /> },
     ];
 
     return (
         <main>
             <section className="min-h-screen flex items-center justify-between px-10 
-      bg-white dark:bg-darkBg text-black dark:text-white transition">
+      bg-white dark:bg-darkBg text-black dark:text-white transition grid xs:grid-row-2  sm:grid-cols-2 ">
 
                 {/* LEFT */}
                 <div>
                     <h1
-                        initial={{ opacity: 0, y: 20 }}
-                        animate={{ opacity: 1, y: 0 }}
+
                         className="text-5xl font-bold">
                         Hi, I'm <span className="text-primary">Prince</span>
                     </h1>
@@ -44,16 +44,16 @@ function Home_3(props) {
                     </p>
 
                     <div className='flex mt-3 text-sm text-gray-500 space-x-3'>
-                        <p className="hover:text-amber-50">
+                        <p className="hover:dark:text-amber-50  hover:text-primary">
                             MongoDB
                         </p>
-                        <p className="hover:text-amber-50">
+                        <p className="hover:dark:text-amber-50  hover:text-primary">
                             • Express
                         </p>
-                        <p className="hover:text-amber-50">
+                        <p className="hover:dark:text-amber-50  hover:text-primary">
                             • React
                         </p>
-                        <p className="hover:text-amber-50">
+                        <p className="hover:dark:text-amber-50  hover:text-primary">
                             • Node
                         </p>
                     </div>
@@ -62,7 +62,7 @@ function Home_3(props) {
                         <button className="bg-primary text-white px-6 py-2 rounded-lg hover:cursor-pointer">
                             Contact Me
                         </button>
-                        <button className="border px-6 py-2 rounded-lg hover:cursor-pointer ">
+                        <button className="border px-6 py-2 rounded-lg hover:cursor-pointer " onClick={cv}>
                             Download CV
                         </button>
                     </div>
@@ -86,74 +86,73 @@ function Home_3(props) {
                 {/* RIGHT IMAGE */}
                 <div
 
-                    className="relative"
+                    className="relative md:w-[380px] md:h-[370px] sm:w-[220px] sm:h-[320px] mx-auto overflow-hidden shadow-lg rounded-full items-center"
                 >
-                    <div className="absolute inset-0 blur-2xl border-4  bg-primary opacity-50 rounded-full"></div>
+                {/* <div className=" inset-0  border-2  bg-cyan-100  rounded-full"></div> */}
+                <div className="absolute inset-0   dark:bg-teal-700 bg-gray-600  blur-2xl scale-120 "></div>
 
                     <img
                         src={Profile}
-                        className="relative w-72 rounded-xl dark:shadow-3xl"
+                        className="relative w-75 rounded-xl dark:shadow-3xl mx-auto"
                     />
                 </div>
-            </section>
-            <section className="py-20 px-6 md:px-16 
-    bg-gray-50 dark:bg-[#0b1215] transition">
+               </section>
+                    <section className="py-20 px-6 md:px-16 bg-gray-50 dark:bg-[#0b1215] transition">
 
-                {/* TITLE */}
-                <div className="text-center mb-14">
-                    <h2 className="text-3xl md:text-4xl font-bold">
-                        My Skills
-                    </h2>
-                    <p className="text-gray-500 dark:text-gray-400 mt-2">
-                        Technologies I work with
-                    </p>
-                </div>
+                        {/* TITLE */}
+                        <div className="text-center mb-14">
+                            <h2 className="text-3xl md:text-4xl font-bold text-primary">
+                                My Skills
+                            </h2>
+                            <p className="text-gray-500 dark:text-gray-400 mt-2">
+                                Technologies I work with
+                            </p>
+                        </div>
 
-                {/* GRID */}
-                <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 gap-6">
+                        {/* GRID */}
+                        <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 gap-6">
 
-                    {skills.map((v, i) => {
-                        const Icon = v.icon;
-
-                        return (
-                            <div
-                                key={i}
-                                initial={{ opacity: 0, y: 20 }}
-                                whileInView={{ opacity: 1, y: 0 }}
-                                whileHover={{ y: -8, scale: 1.05 }}
-                                transition={{ duration: 0.3 }}
-                                className="
+                            {skills.map((v, i) => {
+                               
+                                return (
+                                    <div
+                                        key={i}
+                                        initial={{ opacity: 0, y: 20 }}
+                                        whileInView={{ opacity: 1, y: 0 }}
+                                        whileHover={{ y: -8, scale: 1.05 }}
+                                        transition={{ duration: 0.3 }}
+                                        className="
               group p-6 rounded-2xl text-center cursor-pointer
               bg-white dark:bg-[#111827]
               shadow-sm hover:shadow-lg
               dark:border dark:border-gray-700
               transition duration-300
               "
-                            >
-                                {/* ICON */}
-                                <div className="
+                                    >
+                                        {/* ICON */}
+                                        <div className="
                 flex justify-center mb-4 
                
                 group-hover:scale-110 
                 transition
               ">{
-                                        v.icon
-                                    }
-                                    {/* <Icon size={32} /> */}
-                                </div>
+                                                v.icon
+                                            }
+                                            {/* <Icon size={32} /> */}
+                                        </div>
 
-                                {/* NAME */}
-                                <h3 className="font-semibold text-sm md:text-base text-primary">
-                                    {v.name}
-                                </h3>
-                            </div>
-                        );
-                    })}
+                                        {/* NAME */}
+                                        <h3 className="font-semibold text-sm md:text-base text-primary">
+                                            {v.name}
+                                        </h3>
+                                    </div>
+                                );
+                            })}
 
-                </div>
-            </section>
-        </main>
-    );
+                        </div>
+                    </section>
+                </main>
+                );
 }
 
-export default Home_3;
+                export default Home_3;
